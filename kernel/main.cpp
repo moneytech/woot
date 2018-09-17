739ba4eb2e27b62456dcf6a255c7ecf0d7de7b64
pvc <pvc.meheha@gmail.com>
2018-09-17 16:48:56 +0200

    restored some test code

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -69,9 +69,9 @@ extern "C" int kmain(multiboot_info_t *mbootInfo)
     Thread *ct = Thread::GetCurrent();
     for(;;)
     {
-        //double t = Time::GetSystemUpTime();
+        double t = Time::GetSystemUpTime();
         Time::DateTime dt;
-        //Time::FracUnixToDateTime(t, &dt);
+        Time::FracUnixToDateTime(t, &dt);
         printf("main (runtime %.2d:%.2d:%.2d.%02d)\n", dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
         ct->Sleep(250, false);
     }
