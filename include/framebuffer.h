bbd90f0035d9be767cdadd9b1d45d97ef7828ffa
pvc <pvc.meheha@gmail.com>
2018-11-09 20:41:02 +0100

    removed commented out header code in FrameBuffer class

--- include/framebuffer.h
+++ include/framebuffer.h
@@ -22,31 +22,9 @@ public:
 
         ModeInfo();
         ModeInfo(int width, int height, size_t pitch, PixMap::PixelFormat format);
-        /*int BPP;
-        int AlphaShift, RedShift, GreenShift, BlueShift;
-        int AlphaBits, RedBits, GreenBits, BlueBits;*/
     };
-    /*union Color
-    {
-        uint32_t Value;
-        struct { uint8_t A, R, G, B; };
-
-        Color();
-        Color(const Color &src);
-        Color(uint8_t r, uint8_t g, uint8_t b);
-        Color(uint8_t a, uint8_t r, uint8_t g, uint8_t b);
-        Color(uint32_t value);
-        static Color FromFloatRGB(float r, float g, float b);
-        uint32_t ToValue(ModeInfo &mode);
-        static Color FromValue(ModeInfo &mode, uint32_t value);
-    };*/
 protected:
     int ID;
-    /*union
-    {
-        void *Pixels;
-        uint8_t *PixelBytes;
-    };*/
 public:
     PixMap *Pixels;
 
@@ -55,7 +33,6 @@ public:
     static int Remove(FrameBuffer *fb);
 
     FrameBuffer();
-    //ModeInfo GetMode();
     int Lock();
     void *GetPixels();
     void UnLock();
