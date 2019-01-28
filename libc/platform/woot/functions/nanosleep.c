5fc601bb4df48956fc7a14f269a13931399dbf83
pvc <pvc.meheha@gmail.com>
2019-01-28 10:35:19 +0100

    changing libc yet again this time to newlib

--- libc/platform/woot/functions/nanosleep.c
+++
@@ -1,7 +0,0 @@
-#include <sys/syscall.h>
-#include <time.h>
-
-int nanosleep(const struct timespec *req, struct timespec *rem)
-{
-    return syscall2(SYS_nanosleep, (long)req, (long)rem);
-}
--- libc/platform/woot/functions/times.c
+++
@@ -1,9 +0,0 @@
-#include <errno.h>
-#include <sys/times.h>
-
-clock_t times(struct tms *buf)
-{
-    errno = EINVAL;
-    return (clock_t)-1;
-}
-
